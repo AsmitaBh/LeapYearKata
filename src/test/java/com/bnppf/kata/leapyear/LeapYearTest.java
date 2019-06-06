b64c4b5628ad8420c1336ef5dc7a5ca74e1d3136
Angelet <angelet@abc.com>
2019-06-06 14:35:31 +0200

    Refactored the test case to avoid duplicate class initialization: added @before method

--- src/test/java/com/bnppf/kata/leapyear/LeapYearTest.java
+++ src/test/java/com/bnppf/kata/leapyear/LeapYearTest.java
@@ -3,19 +3,25 @@ package com.bnppf.kata.leapyear;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class LeapYearTest {
 	
+	private LeapYear leapYear;
+
+	@Before
+	public void setUp() throws Exception {
+		leapYear = new LeapYear();
+	}
+	
 	@Test
 	public void when2000ThenLeapYear() {
-		LeapYear leapYear = new LeapYear();
 		assertTrue(leapYear.isLeapYear(2000));
 	}
 	
 	@Test
 	public void when2001ThenNonLeapYear() {
-		LeapYear leapYear = new LeapYear();
 		assertFalse(leapYear.isLeapYear(2001));
 	}
 
